05dbaafc55ad82d7a0a4fdc2ad5a8d0a538ec537
tudorgonta <55688652+tudorgonta@users.noreply.github.com>
2022-10-20 20:13:13 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@
       };
       const parser = new XMLParser(options);
       try {
-      await axios.get('https://vue-project-pearl.vercel.app/src/assets/data.xml', { crossdomain: true })
+      await axios.get('/src/assets/data.xml', { validateStatus: false })
         .then(response => {
           var xmlText = response.data
           var result1 = parser.parse(xmlText)
